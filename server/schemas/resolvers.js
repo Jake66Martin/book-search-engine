a6fb4cafcba2696681aee66a9815e704c7c6005a
leviathan <jake66martin@hotmail.com>
2024-01-17 12:14:24 -0500

    trying to create resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,21 @@
-const { Profile } = require('../models');
+const { User } = require('../models/User');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
 
     Query: {
+
+        getSingleUser: async (parent, { user, params }) => {
+            const foundUser = await User.findOne({
+              $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+            });
+      
+            if (!foundUser) {
+              throw new Error('Cannot find a user with this id!');
+            }
+      
+            return foundUser;
+          },
 
     },
 
