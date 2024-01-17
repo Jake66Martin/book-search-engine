3bcbb489ef045be99a340f3d2ba75a46c5307111
leviathan <jake66martin@hotmail.com>
2024-01-17 17:17:34 -0500

    added mutations to front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,58 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+
+mutation addUser($userName: String!, $email: String!, $password: String!) {
+    addUser(userName: $userName, email: $email, password: $password) {
+      token
+      user {
+        _id
+        email
+        password
+        savedbooks {
+          
+        }
+        username
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        email
+        password
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook {
+    saveBook {
+      _id
+      savedbooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      savedbooks {
+        bookId
+      }
+    }
+  }
+`;
