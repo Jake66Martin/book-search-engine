39bcf430b60b7d1fc682575a90e9f866860a2477
leviathan <jake66martin@hotmail.com>
2024-01-21 14:09:38 -0500

    made changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect("mongodb+srv://jake66martin:jakesdu1@jakecluser.dcwjdeg.mongodb.net/googlebooks?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
