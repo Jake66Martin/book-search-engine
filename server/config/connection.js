e1f752e1e8ec3984c6af134d50d16efa92f114eb
leviathan <jake66martin@hotmail.com>
2024-01-21 14:27:16 -0500

    added env

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://jake66martin:jakesdu1@jakecluser.dcwjdeg.mongodb.net/googlebooks?retryWrites=true&w=majority" || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
